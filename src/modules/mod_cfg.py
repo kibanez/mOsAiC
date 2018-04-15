c03931d76e2e9d8a4b6e344ab7f4ae148f87efb4
kibanez <kristina.ibanez85@gmail.com>
2018-04-15 12:22:48 +0100

    clean up code

--- src/modules/mod_cfg.py
+++ src/modules/mod_cfg.py
@@ -1,42 +1,44 @@
-'''
+#!/usr/bin/
+"""
 Created on 23/01/2015
 
 @author: adelpozo
-'''
+"""
 
 
-#!/usr/bin/python
+import sys
+import re
+import os
+import string
+import urllib
+import time
+import math
+import random
 
-import sys, re, os, string, urllib, time, math, random
-
-import  ConfigParser
+import ConfigParser
 
 #######################################################################
 
+
 def read_cfg_file(cfg_filename):
-    
-    fi = open(cfg_filename,'r')
+    """
+
+    :param cfg_filename:
+    :return:
+    """
+    fi = open(cfg_filename, 'r')
     
     config = ConfigParser.ConfigParser()
     config.readfp(fi)
     
     hash_cfg = {}
-        
-    for field in config.options('INPUT'):
-        hash_cfg[field] = config.get('INPUT',field)
-    
-    for field in config.options('OUTPUT'):
-        hash_cfg[field] = config.get('OUTPUT',field)
-        
-    for field in config.options('SOFTWARE'):
-        hash_cfg[field] = config.get('SOFTWARE',field)
-    
-    for field in config.options('REFERENCE'):
-        hash_cfg[field] = config.get('REFERENCE',field)
-     
+    for field in ['INPUT', 'OUTPUT', 'SOFTWARE', 'REFERENCE']:
+        for field_value in config.options(field):
+            hash_cfg[field_value] = config.get(field, field_value)
+
     if config.has_section('CONTROLS'):
         hash_cfg['controls'] = config.items('CONTROLS')
-        
+
     fi.close()
     
     return hash_cfg
